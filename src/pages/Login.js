c9c710b39c079478185784c72b92e5125200c49b
Harshvardhansinh Sarvaiya <145086716+hdsarvaiya@users.noreply.github.com>
2025-02-10 23:58:59 +0530

    Update Login.js

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -15,7 +15,7 @@ const Login = () => {
     const handleSubmit = async (e) => {
       e.preventDefault();
       try {
-        const res = await axios.post("http://localhost:5000/api/auth/login", formData);
+        const res = await axios.post("https://landt-maintain-production.up.railway.app/api/auth/login", formData);
         localStorage.setItem("token", res.data.token);
         const user = jwtDecode(res.data.token);
         alert(`Welcome, ${user.name}`);
