acb7d41fd6578c37ae9e05f0b6d8252d29472905
Harshvardhansinh Sarvaiya <145086716+hdsarvaiya@users.noreply.github.com>
2025-02-10 23:59:18 +0530

    Update Register.js

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -14,7 +14,7 @@ const Register = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      await axios.post("http://localhost:5000/api/auth/register", formData);
+      await axios.post("https://landt-maintain-production.up.railway.app/api/auth/register", formData);
       alert("Registration Successful!");
       navigate("/");
     } catch (err) {
